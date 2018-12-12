51ac4a855307ac7d3f3d601858b8ecded44605bf
Khazin <45104127+KhazinAnger@users.noreply.github.com>
2018-12-12 14:29:50 -0700

    little nothing

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Lets play a game\n\n")
+
+x = "0"
+while x :
+    x = input("\nchoose a class ")
+    if x == 'w':
+        print("\nYour a wizard")
+    if x == 's':
+        print("\nYour a solder")
+    if x == 'r':
+        print("\nYour a rogue")
